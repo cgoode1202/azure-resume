9c1ade6a94ba2156792e9aadbd94a30caabb0c0e
cgoode1202 <cgoode1202@gmail.com>
2023-04-27 11:35:57 -0700

    updated backend files, still in progress

---
+++ backend/api/GetResumeCounter.cs
@@ -0,0 +1,33 @@
+using System.Net;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Logging;
+
+namespace Company.Function
+{
+    public class GetResumeCounter
+    {
+        private readonly ILogger _logger;
+
+        public GetResumeCounter(ILoggerFactory loggerFactory)
+        {
+            _logger = loggerFactory.CreateLogger<GetResumeCounter>();
+        }
+
+        [Function("GetResumeCounter")]
+        public HttpResponseData Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequestData req,
+            [CosmosDB(databaseName:"AzureResume", collectionName: "Counter", ConnectionStringSetting = "AzureResumeConnectionString", Id = "1")] Counter counter)
+        {
+            _logger.LogInformation("C# HTTP trigger function processed a request.");
+
+            var response = req.CreateResponse(HttpStatusCode.OK);
+            response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
+
+            response.WriteString("Welcome to Azure Functions!");
+
+            return response;
+        }
+    }
+}
